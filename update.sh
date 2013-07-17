5b516dad918d410d257decb4c250b91b28926e18
Romans Malinovskis <r@agiletech.ie>
2013-07-17 18:20:56 +0100

    added updater

---
+++ update.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# This script updates dependencies of your projects and performs necessary migration.
+# You should call this script after you perform git pull
+
+RED=$(printf "\x1b[31m")
+NORMAL=$(printf "\x1b[0m")
+
+function say {
+    echo -n " =>" $RED
+    echo -n $*
+    echo $NORMAL
+}
+
+
+say "Updating main repository"
+
+git pull
+
+say "Calling Composer PHP to update dependencies"
+
+# Update dependencies
+if [ -f composer.json ]; then
+   [ -f composer.phar ] || curl -sS https://getcomposer.org/installer | php
+   php composer.phar update
+fi
+
+say "Updating Agile Toolkit"
+( cd atk4; git pull origin master )
+
+say "Updating Agile Toolkit Old Addons"
+( cd atk4-addons; git pull origin master )
+
+say "You are up-to-date!!!";
